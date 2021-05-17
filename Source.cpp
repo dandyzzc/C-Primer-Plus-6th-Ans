e5f59ee1c906b615e3fbd64700cc2ba86894dba6
dandyzzc <65354319+dandyzzc@users.noreply.github.com>
2021-05-17 21:11:40 +0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string Name = "Dandy";
+	string Address = "HongKong PolyU";
+	cout <<"Name: " <<Name << endl;
+	cout <<"Address: " <<Address << endl;
+	return 0;
+}
