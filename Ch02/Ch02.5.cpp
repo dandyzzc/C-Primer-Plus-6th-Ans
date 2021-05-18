cff0774fcb2b8d1ff972b6e9c1637e53c05c8310
dandyzzc <65354319+dandyzzc@users.noreply.github.com>
2021-05-18 14:00:09 +0800

    Add files via upload

---
+++ Ch02/Ch02.5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+double Convert(double Celsius);
+
+int main()
+{
+	double Celsius;
+	cout << " Please enter a Celsius value: ";
+	cin >> Celsius;
+	cout << Celsius << " degrees Celsius is " << Convert(Celsius) << " degrees Fahrenheit. " << endl;
+
+
+}
+double Convert(double Celsius)
+{
+	double Fahrenheit;
+	Fahrenheit = 1.8*Celsius + 32.0;
+	return Fahrenheit;
+}
