ee0657d124df52cd48d7800fb10dcafca2595c5e
dandyzzc <65354319+dandyzzc@users.noreply.github.com>
2021-06-04 18:16:45 +0800

    Add files via upload

---
+++ Ch05.1.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+
+int main()
+{
+	int a, b = 0;
+	int sum = 0;
+	cout << "Please enter two integer: "<<endl;
+	cout << "Enter the first number: _\b";
+	cin >> a;
+	cout << "Enter the second number: _\b";
+	cin >> b;
+
+	for (int i = a; i <= b; i++)
+	{
+		sum += i;
+	}
+	cout << "The total sum of " << a << " to " << b << " is " << sum<<endl;
+	
+	
+	return 0;
+}
